56b31e23cb18dad2f2098dd68fdbecac8ec5d495
malykhs1 <malykhs1955@yandex.ru>
2022-05-04 17:13:47 +0300

    About me fix

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ export const AboutMe = () => {
             <div className="biography_place_text">
             <h3 className="biography_place_text__name">Alex Malykh</h3>
             <p className="biography_place_text__job">Front-end developer</p>
-            <p className="biography_place_text__bio-text">I graduated from the Pedagogical University, but now I am building a career in the field of web development. I got development skills in a ten-month Yandex bootcapm. I like neurophysiology, the acquired knowledge helps me to effectively build the educational process, which allowes me not to go crazy during learnong web development. I practice yoga and meditation. I am crazy about football and Tel Aviv food.</p>
+            <p className="biography_place_text__bio-text">I graduated from the Pedagogical University, but now I am building a career in the field of web development. I got development skills in a ten-month Yandex bootcapm. I like neurophysiology, the acquired knowledge helps me to effectively build the educational process, which allowes me not to go crazy during learning web development. I practice yoga and meditation. I am crazy about football and Tel Aviv food.</p>
             <div className="biography_place_text__links">
                 <a href="#" className="biography_place__link">Facebook</a>
                 <a href="#" className="biography_place__link">Github</a>
